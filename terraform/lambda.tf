16aef06271429bfe82ee0f8197d0d619f4392d22
Colin Duffy <colinduffy@syn-172-100-008-010.res.spectrum.com>
2025-03-28 12:21:18 -0400

    Fix formtatting in lambda.tf for Github Actions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "main_lambda" {
   function_name = "main_lambda"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "index.handler"           # Adjust if using a different entry point
-  runtime       = "python3.11"              # Change if using Node.js, etc.
+  handler       = "index.handler" # Adjust if using a different entry point
+  runtime       = "python3.11"    # Change if using Node.js, etc.
   filename      = "${path.module}/lambda_function_payload.zip"
 
   source_code_hash = filebase64sha256("${path.module}/lambda_function_payload.zip")
