9454ff9f000bc1af7c358b5ad729032c91015703
Colin Duffy <colinduffy@syn-172-100-008-010.res.spectrum.com>
2025-03-28 12:36:19 -0400

    Move variables.tf into terraform/ so terraform can see it

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,11 @@
-# AWS region
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"
-}
-
-# S3 bucket name for Lambda to use
 variable "s3_bucket_name" {
+  type        = string
   description = "Name of the S3 bucket used by the Lambda function"
-  type        = string
 }
 
-# Lambda function name
-variable "lambda_function_name" {
-  description = "The name of the Lambda function"
+variable "blockchain_admin_password" {
   type        = string
-  default     = "main_lambda"
+  description = "Fabric admin password for the Managed Blockchain member"
+  sensitive   = true
 }
 
-# Lambda runtime (optional override)
-variable "lambda_runtime" {
-  description = "The runtime environment for the Lambda function"
-  type        = string
-  default     = "python3.11"
-}
-
-# Lambda handler (optional override)
-variable "lambda_handler" {
-  description = "The function entrypoint in your Lambda code"
-  type        = string
-  default     = "index.handler"
-}
-
-# Environment stage
-variable "env" {
-  description = "Deployment environment name"
-  type        = string
-  default     = "dev"
-}
